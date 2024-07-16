50e1e40306f1946c576ff72e58f6ef09ad908e63
Long Pham <long.pham.au@gmail.com>
2024-07-16 22:51:46 +1000

    Fix formatting

--- src/test/java/au/com/weather/service/RateLimitServiceTest.java
+++ src/test/java/au/com/weather/service/RateLimitServiceTest.java
@@ -7,27 +7,25 @@ import org.junit.jupiter.api.Test;
 
 public class RateLimitServiceTest {
 
-	
-	RateLimitServiceImpl rateLimitService = new RateLimitServiceImpl();
-	
-	
-	@Test
-	public void testAllowRequest() {
-		// configure to allow x3 calls every minute for each api key
-		rateLimitService.callLimit = 2;
-		rateLimitService.duration = 1;
-		
-		assertTrue(rateLimitService.allowRequest("1234"));
-		assertTrue(rateLimitService.allowRequest("1234"));
-		assertFalse(rateLimitService.allowRequest("1234"));
-		
-		assertTrue(rateLimitService.allowRequest("4567"));
-		assertTrue(rateLimitService.allowRequest("4567"));
-		assertFalse(rateLimitService.allowRequest("4567"));
-		
-		assertTrue(rateLimitService.allowRequest("8910"));
-		assertTrue(rateLimitService.allowRequest("8910"));
-		assertFalse(rateLimitService.allowRequest("8910"));
-	}
-	
+    RateLimitServiceImpl rateLimitService = new RateLimitServiceImpl();
+
+    @Test
+    public void testAllowRequest() {
+        // configure to allow x3 calls every minute for each api key
+        rateLimitService.callLimit = 2;
+        rateLimitService.duration = 1;
+
+        assertTrue(rateLimitService.allowRequest("1234"));
+        assertTrue(rateLimitService.allowRequest("1234"));
+        assertFalse(rateLimitService.allowRequest("1234"));
+
+        assertTrue(rateLimitService.allowRequest("4567"));
+        assertTrue(rateLimitService.allowRequest("4567"));
+        assertFalse(rateLimitService.allowRequest("4567"));
+
+        assertTrue(rateLimitService.allowRequest("8910"));
+        assertTrue(rateLimitService.allowRequest("8910"));
+        assertFalse(rateLimitService.allowRequest("8910"));
+    }
+
 }
